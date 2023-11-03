4df8d48e088bd3250016234cf743bdccf4678b64
JuhoR <60245517+DeveloperJuho@users.noreply.github.com>
2023-11-03 13:21:28 +0200

    CompleteRockPaperScissors_NoGUI

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // global variables
-
-// For now let's keep player selection as rock
-
-const playerChoice = "Rock";
+let yourScore = 0;
+let opponentScore = 0;
 
 // Play a round of RPS
 
@@ -26,24 +24,36 @@ function playRound(playerSelection, computerSelection){
     // Functionality for selecting rock
     if(playerSelection == "rock".toUpperCase()){
         // no need to uppercase this as these are hardcoded for computer, same goes for the rest of these
-        if(computerSelection == "Scissors") return "Opponent chose Scissors. You Win!";
+        if(computerSelection == "Scissors"){
+            yourScore++;
+            return "Opponent chose Scissors. You Win!";
+        }
         // no need for else-clause as this will be either resolved during if or it will land here
+        opponentScore++;
         return "Opponent chose Paper. You Lose!";
     }
 
     // Functionality for selecting paper
     else if(playerSelection == "paper".toUpperCase()){
         // no need to uppercase this as these are hardcoded for computer, same goes for the rest of these
-        if(computerSelection == "Scissors") return "Opponent chose Scissors. You Lose!";
+        if(computerSelection == "Scissors"){
+            opponentScore++;
+            return "Opponent chose Scissors. You Lose!";
+        } 
         // no need for else-clause as this will be either resolved during if or it will land here
+        yourScore++;
         return "Opponent chose Rock. You Win!";
     }
 
     // Functionality for selecting scissors
     else if(playerSelection == "scissors".toUpperCase()){
         // no need to uppercase this as these are hardcoded for computer, same goes for the rest of these
-        if(computerSelection == "Rock") return "Opponent chose Rock. You Lose!";
+        if(computerSelection == "Rock"){
+            opponentScore++;
+            return "Opponent chose Rock. You Lose!";
+        } 
         // no need for else-clause as this will be either resolved during if or it will land here
+        yourScore++;
         return "Opponent chose Paper. You Win!";
     }
 
@@ -56,9 +66,9 @@ function playRound(playerSelection, computerSelection){
 
 function getComputerChoice(){
     let computerChoice = 0;
-
+    // Randomize the choice
     let x = Math.floor(Math.random()*3);
-
+    // Select correct choice according to randomizer, also unnecessary as numbers would have worked in playRound function too
     switch(x){
         case 0:
             computerChoice = "Scissors";
@@ -73,11 +83,57 @@ function getComputerChoice(){
             console.log("Something went wrong");
             break;
     }
-
-
+    // Return the choice
     return computerChoice;
 }
 
+// Players choice, in a separate function so that the choice will be in correct form and for clarity
 
+function playersChoice(){
+    let correctForm = false;
+    let choice;
+    // Loop until player's prompt is passed
+    while(correctForm == false){
+        choice = prompt("Rock, paper or scissors?");
+        choice = String(choice).toUpperCase();
+        if (choice == "ROCK" || choice == "PAPER" || choice == "SCISSORS"){
+            correctForm = true;
+        }
+    }
+    
 
-console.log(getComputerChoice());
+    return choice;
+}
+
+// Actual game function
+
+function game(){
+    // Nullify scores
+    opponentScore = 0;
+    yourScore = 0;
+
+    // Initalize playerchoice
+    let playerChoice = 0;
+
+    // Play five rounds
+    for (let i = 0; i < 5; i++){
+        playerChoice = playersChoice();
+        console.log(playRound(playerChoice, getComputerChoice()));
+
+        console.log(`Your point total is ${yourScore} and computers point total is ${opponentScore}`);
+    }
+
+    if (opponentScore > yourScore){
+        console.log(`You lost the game with score of ${yourScore} - ${opponentScore}`);
+    }
+    else if (opponentScore < yourScore){
+        console.log(`You won the game with score of ${yourScore} - ${opponentScore}`);
+    }
+    else{
+        console.log(`Game was a draw with score of ${yourScore} - ${opponentScore}`);
+    }
+
+    console.log("If you want to play again, type game() in to the browsers console");
+}
+
+game();
